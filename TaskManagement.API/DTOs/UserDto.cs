495bb04d0f5c0e2d73ee715e2f8e2c2f362a88b3
dabananda <dabananda.dev@gmail.com>
2025-08-10 11:18:49 +0600

    refactored some use cases

--- TaskManagement.API/DTOs/UserDto.cs
+++ TaskManagement.API/DTOs/UserDto.cs
@@ -3,6 +3,7 @@
     public class UserDto
     {
         public Guid Id { get; set; }
-        public string FullName { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
     }
 }
