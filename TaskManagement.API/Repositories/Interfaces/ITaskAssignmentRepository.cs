9dc882aa94df784f03de3b3657cd61044641228e
dabananda <dabananda.dev@gmail.com>
2025-08-12 13:39:38 +0600

    feat: interface and repository for task assigments

---
+++ TaskManagement.API/Repositories/Interfaces/ITaskAssignmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskManagement.API.Models;
+
+namespace TaskManagement.API.Repositories.Interfaces
+{
+    public interface ITaskAssignmentRepository
+    {
+        Task<IEnumerable<TaskAssignment>> GetAllAsync();
+        Task<TaskAssignment?> GetByIdAsync(Guid id);
+        Task AddAsync(TaskAssignment assignment);
+        void Remove(TaskAssignment assignment);
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ TaskManagement.API/Repositories/TaskAssignmentRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.API.Data;
+using TaskManagement.API.Models;
+
+namespace TaskManagement.API.Repositories
+{
+    public class TaskAssignmentRepository
+    {
+        private readonly TaskManagementDbContext _context;
+
+        public TaskAssignmentRepository(TaskManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TaskAssignment>> GetAllAsync()
+        {
+            return await _context.TaskAssignments
+                .Include(t => t.TaskItem)
+                .Include(t => t.User)
+                .ToListAsync();
+        }
+
+        public async Task<TaskAssignment?> GetByIdAsync(Guid id)
+        {
+            return await _context.TaskAssignments
+                .Include(t => t.TaskItem)
+                .Include(t => t.User)
+                .FirstOrDefaultAsync(x => x.UserId == id);
+        }
+
+        public async Task AddAsync(TaskAssignment assignment)
+        {
+            await _context.TaskAssignments.AddAsync(assignment);
+        }
+
+        public void Remove(TaskAssignment assignment)
+        {
+            _context.TaskAssignments.Remove(assignment);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
